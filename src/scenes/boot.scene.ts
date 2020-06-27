aab7182b35d437ec5e82535211de900f50778fc9
triamero <4444613+triamero@users.noreply.github.com>
2020-06-27 11:46:12 +0300

    * fix assets path

--- src/scenes/boot.scene.ts
+++ src/scenes/boot.scene.ts
@@ -3,8 +3,8 @@ import {Difficulty, GameConfig} from "@m8/core";
 export class BootScene extends Phaser.Scene {
 
     preload() {
-        this.load.image("menu-button", "assets/menu-button.png");
-        this.load.image("square-button", "assets/square.png");
+        this.load.image("menu-button", "dist/menu-button.png");
+        this.load.image("square-button", "dist/square.png");
     }
 
     create() {
