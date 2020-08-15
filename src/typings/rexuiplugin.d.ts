0f5e3e44decdba0af85e850134048c21a4faf421
triamero <4444613+triamero@users.noreply.github.com>
2020-08-16 02:06:11 +0300

    * remove unused typings from rexuiplugins.d.ts

--- src/typings/rexuiplugin.d.ts
+++ src/typings/rexuiplugin.d.ts
@@ -7,79 +7,14 @@ declare module "phaser" {
     interface RexUIPlugins {
         add: RexUIGameObjectFactory;
     }
-
-    namespace Plugins {
-        namespace GameObjects {
-            class ContainerBase extends Phaser.GameObjects.Zone {
-                constructor(scene: Phaser.Scene, x: number, y: number, width: number, height: number);
-
-                destroy(fromScene?: boolean): void;
-
-                contains(gameObject: Phaser.GameObjects.GameObject): boolean;
-
-                add(gameObjects: Phaser.GameObjects.GameObject[]): this;
-
-                remove(gameObjects: Phaser.GameObjects.GameObject[], destroyChild: boolean): this;
-
-                clear(destroyChild: boolean): this;
-            }
-
-            class ContainerLite extends ContainerBase {
-                constructor(scene: Phaser.Scene, x: number, y: number, width: number, height: number, children: Phaser.GameObjects.GameObject[])
-            }
-        }
-    }
 }
 
 interface RexUIGameObjectFactory {
     label(config: RexUILabelConfig): any;
 
-    scrollablePanel(config: RexUIScrollablePanelConfig): RexUIScrollablePanel;
-
-    sizer(config: any): any;
-
-    gridSizer(config: RexUIGridSizerConfig): RexUIGridSizer;
-
     roundRectangle(x: number, y: number, width: number, height: number, radius: number, color?: number | string): any;
 
-    fixWidthSizer(config: RexUIFixWidthSizerConfig): any;
-
     gridTable(config: any): any;
-}
-
-interface RexUIScrollablePanel {
-    layout(): this;
-}
-
-interface RexUIScrollablePanelConfig {
-    x: number;
-    y: number;
-
-    width: number;
-    height: number;
-
-    scrollMode: number;
-
-    background?: any;
-
-    panel?: any;
-
-    scroller?: {
-        threshold: number;
-        slidingDeceleration: number;
-        backDeceleration: number;
-    };
-
-    slider?: any;
-
-    space: {
-        left?: number;
-        right?: number;
-        top?: number;
-        bottom?: number;
-
-        panel?: number;
-    }
 }
 
 interface RexUILabelConfig {
@@ -97,34 +32,4 @@ interface RexUILabelConfig {
         top?: number;
         bottom?: number;
     }
-}
-
-interface RexUIFixWidthSizerConfig {
-    space: {
-        left?: number;
-        right?: number;
-        top?: number;
-        bottom?: number;
-
-        panel?: number;
-
-        item?: number;
-        line?: number;
-    }
-}
-
-interface RexUIGridSizerConfig {
-    x?: number;
-    y?: number;
-    width?: number;
-    height?: number;
-
-    anchor?: number;
-
-    column: number;
-    row: number;
-}
-
-interface RexUIGridSizer {
-    add(gameObject: Phaser.GameObjects.GameObject, column: number, row: number, align?: string, padding?: number, expand?: boolean, key?: string): this;
 }
